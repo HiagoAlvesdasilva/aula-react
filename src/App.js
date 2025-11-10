942579b212800ebd7f35259c3dc11b8ac6ebec5c
csivdcifba <hiagoalvesdasilva11@gmail.com>
2025-11-10 16:21:17 -0300

    feat: adicionando opçoes do menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import imgPerfil from './imagens/perfil.svg';
+import imgSacola from './imagens/sacola.svg';
 
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const iconesOpcoes = [imgPerfil, imgSacola];
 function App() {
   return (
-    <div>
-      <header>
-        <Logo></Logo>
+    <div className='App'>
+      <header className='App-header'>
+      <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ))}  
+          {iconesOpcoes.map((icone, index) => (
+            <li key={index} className='icone'>
+            <img src={icone} alt={`icone-${index}`} />
+          </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -3,8 +3,8 @@ import logo from '../../imagens/logo.svg'
 
 function Logo(){ 
     return(
-    <div className="logo">
-      <img src={logo} alt="logo" />
+    <div className='logo'>
+      <img src={logo} alt='logo' />
       <p><strong>Hiago Store</strong></p>
     </div>
     )
