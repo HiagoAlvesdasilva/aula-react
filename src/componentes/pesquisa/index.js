b04567b94005276af2f7f2c054153a78aa6b5c7c
csivdcifba <hiagoalvesdasilva11@gmail.com>
2025-11-17 16:31:17 -0300

    feat: usando useState
    falta ajustar Dados pesquisa Revover da pasta e ajustar tbm o pesquisa

--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -2,18 +2,34 @@
 import PesquisaContainer from '../PesquisaContainer';
 import SubTitulo from '../SubTitulo';
 import Titulo from '../Titulo';
+import {livros} from './DadosPesquisa/DadosPesquisa';
 import './estilo.css';
 
 import { useState } from 'react';
 function Pesquisa() {
-  const [textoDigitado, setTextoDigitado] = useState('');
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
   return (
     <PesquisaContainer>
         <Titulo>Ja sabe por onde começar?</Titulo>
         <SubTitulo>Encontre seu produto</SubTitulo>
-    <input className="input" placeholder="Digite o nome do produto" onBlur={evento => setTextoDigitado(evento.target.value)}/>
+    <input className="input" placeholder="Digite o nome do produto" 
+    onBlur={evento => { 
+    const textoDigitado = evento.target.value
+    const resultadoPesquisa = livros.filter(
+      livro => livro.nome.includes(textoDigitado)
+    )
+    setLivrosPesquisados(resultadoPesquisa);
+    console.log(livrosPesquisados)
+    }}
+    />
+    {livrosPesquisados.map(livro => (
+      <div>
+        <p>{}livro.nome</p>
+        <img src={livro.src} alt='livro'></img>
+      </div>
+    ))}
     </PesquisaContainer>
-    
+
   );
 }
 
