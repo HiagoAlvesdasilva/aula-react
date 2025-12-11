2cf5a05d923dd5af7e6a83a9e5a8cc7eb5209bec
csivdcifba <hiagoalvesdasilva11@gmail.com>
2025-12-11 11:56:58 -0300

    fix: ajustando as busca para o BD

--- src/componentes/PesquisaContainer/index.js
+++ src/componentes/PesquisaContainer/index.js
@@ -1,13 +1,23 @@
-
+import './estilo.css';
 import SubTitulo from '../SubTitulo';
 import Titulo from '../Titulo';
-import {livros} from './DadosPesquisa';
-import './estilo.css';
-import Resultado from '../Resultado'
+import Resultado from '../Resultado';
+import { useEffect, useState } from 'react';
+import {getLivros} from '../../servicos/livros';
 
-import { useState } from 'react';
 function PesquisaContainer() {
   const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+  const [livros, setLivros] = useState([]);
+
+  useEffect (() => {
+    fetchLivros()
+  }, [])
+
+  async function fetchLivros(){
+    const livrosDaAPI = await getLivros()
+    setLivros(livrosDaAPI);
+  }
+
   return (
     <section className = 'PesquisaContainer'>
         <Titulo>Ja sabe por onde começar?</Titulo>
