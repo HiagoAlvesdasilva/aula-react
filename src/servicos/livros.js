4aa8fc00ce301b83379e3f4aa63bbea6495a14c5
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-12-14 22:15:50 -0300

    fix: ajuste

--- src/servicos/livros.js
+++ src/servicos/livros.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const livrosAPI = axios.create({baseURL:"http://localhost:8000/livros"})
 
 function getLivros(){
-    const response = livrosAPI.get('/')
+    const response = livrosAPI.get("/")
     return response.data
 }
 
